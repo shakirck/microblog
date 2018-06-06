e460a2c629ea87c55b4fd4ec3621e8aabfa8bbeb
shakirck <shakirck333@gmail.com>
2018-06-06 15:12:59 +0530

    completed chapter 2 Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app 
-
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return "I thought I wsnted a career, turns out I just wanted pay checks"
+    user ={'username':'Shakir'}
+    posts= [
+
+        {
+            'author':{'username':'john'},
+            'body':'this is good idea'
+        },
+        {
+            'author':{'username':'mary'},
+            'body':'Great idea to aquire Github'
+        }
+    ]
+    return render_template('index.html',user=user,posts=posts)
